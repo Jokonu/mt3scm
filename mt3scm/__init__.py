db98ca2bf786a9f99545cc284bd87adf1bc73ab8
Jonas Köhne <jonas.koehne@iav.de>
2022-05-06 16:15:46 +0200

    correct version bump now

--- mt3scm/__init__.py
+++ mt3scm/__init__.py
@@ -3,7 +3,7 @@ MT3SCM
 ======
 This module provides 'Multivariate Time Series Sub-Sequence CLustering Metric' computation function
 """
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 from .mt3scm_computation import MT3SCM, mt3scm_score
 
